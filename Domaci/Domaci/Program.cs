272af332d1929938d895b0a88d141d1d1b9144ad
AlexSensei <antishadow15@gmail.com>
2017-03-23 00:14:54 +0100

    Dodat domaci

---
+++ Domaci/Domaci/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domaci
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //TemperatureConvertor TC = new TemperatureConvertor();
+            int Option = 1;
+            while (Option != 0)
+            {
+                menu();
+                Option = Convert.ToInt32(System.Console.ReadLine());
+                double tmp;
+
+                switch (Option)
+                {
+                    case 1: 
+                        Console.WriteLine("Input temperature value in farenheit:");
+                        try
+                        {
+                            tmp = Convert.ToDouble(Console.ReadLine());
+                            TemperatureConvertor TC = new TemperatureConvertor(tmp);
+                            Console.WriteLine(TC.CelsiusToFaren());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Greska u parametrima!");
+                        }
+                        break;
+                    case 2: 
+                        Console.WriteLine("Input temperature value in celsius:");
+                        try
+                        {
+                            tmp = Convert.ToDouble(Console.ReadLine());
+                            TemperatureConvertor TC1 = new TemperatureConvertor(tmp);
+                            Console.WriteLine(TC1.FarenToCelsius());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Greska u parametrima!");
+                        }
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        static void menu()
+        {
+            Console.WriteLine("Options:");
+            Console.WriteLine("1)Celsius to Farenheit");
+            Console.WriteLine("2)Farenheit to celsius");
+            Console.WriteLine("0)Exit");
+        }
+    }
+}
---
+++ Domaci/Domaci/TemperatureConvertor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domaci
+{
+    class TemperatureConvertor
+    {
+        public double tmp;
+        public double Tmp
+        {
+            get { return tmp; }
+            private set { Tmp = value; }
+        }
+
+        public TemperatureConvertor(double tmp)
+        {
+            this.tmp = tmp;
+        }
+
+        public double FarenToCelsius()
+        {
+
+            tmp = (tmp - 32) / 1.8;
+            return tmp;
+        }
+        public double CelsiusToFaren()
+        {
+            tmp = tmp * 9 / 5 + 32;
+            return tmp;
+        }
+    }
+}
